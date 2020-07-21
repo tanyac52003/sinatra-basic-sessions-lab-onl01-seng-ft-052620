34e4917f31e4abbb4c7f02dc350f6dbc82b35f50
Tanya Hodges <tanyac52003@gmail.com>
2020-07-21 22:20:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,8 +13,8 @@ class App < Sinatra::Base
   end 
   
   post '/checkout' do 
-    @sessions = session 
+    @sessions = session
     item = params["item"]
-    @sessions[:item] = item 
+    @sessions[:item] = item
   end 
 end
